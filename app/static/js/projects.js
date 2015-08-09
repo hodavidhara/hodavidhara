1f7a05a198d9179ab092767cd2f7b755fd8b2fd3
David Hara <hakuhen@gmail.com>
2015-08-09 18:26:45 -0500

    some cleanup

--- app/static/js/projects.js
+++ app/static/js/projects.js
@@ -4,8 +4,7 @@ var _ = require('lodash');
 var dom = require('domquery');
 var domReady = require('domready');
 
-request({url: '/api/projects', json: true}, function(
-    err, res, body) {
+request({url: '/api/projects', json: true}, function(err, res, body) {
     domReady(function () {
         var projectContainer = dom('#projects');
 
