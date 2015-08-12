d7c0940f3c9e106085f0c9040dff67d4cff41d7c
David Hara <hakuhen@gmail.com>
2015-08-12 01:01:17 -0500

    clean up in app, and script to load apod data into ES

---
+++ scripts/apodesloader.js
@@ -0,0 +1,40 @@
+var elasticsearch = require('elasticsearch');
+var apod = require('nasa-apod');
+var moment = require('moment');
+
+var date = moment();
+
+var client = new elasticsearch.Client({
+    host: 'es.hodavidhara.com',
+    apiVersion: '1.7'
+});
+
+var apodClient = new apod.Client({
+    apiKey: 'OCmskm6eTVLx9NoSVaxXzwJd3yYsP0cNKdSTzj4e'
+});
+
+var _indexPicture = function (picture) {
+    console.log('indexing apod for', picture.date);
+
+    return client.index({
+        index: 'apod',
+        type: 'picture',
+        body: picture
+    })
+};
+
+(function loop() {
+
+    console.log('getting apod for ', date.format('YYYY-MM-DD'));
+
+    apodClient(date).then(function (picture) {
+        picture = JSON.parse(picture);
+        picture.date = date.format('YYYY-MM-DD');
+        return picture;
+    }).then(_indexPicture).then(function () {
+        date.subtract(1, 'days');
+        setTimeout(loop, 500);
+    }).catch(function (err) {
+        console.log(err);
+    });
+})();
