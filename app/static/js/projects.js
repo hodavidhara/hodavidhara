ff6e91c41433e5953117769cbc9d886451dedc73
David Hara <hakuhen@gmail.com>
2015-09-28 23:32:52 -0700

    serving static files from nginx

--- app/static/js/projects.js
+++ app/static/js/projects.js
@@ -1,6 +1,5 @@
 var request = require('browser-request');
 var ProjectCard = require('./hodavidhara/projectcard');
-var _ = require('lodash');
 var dom = require('domquery');
 var domReady = require('domready');
 
@@ -8,7 +7,7 @@ request({url: '/api/projects', json: true}, function(err, res, body) {
     domReady(function () {
         var projectContainer = dom('#projects');
 
-        _.forEach(body, function(projectData) {
+        body.forEach(function(projectData) {
             var card = new ProjectCard({
                 data: projectData
             });
